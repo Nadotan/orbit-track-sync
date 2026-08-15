a4234ccd4837515e24e637db54ad2da885721799
Nadotan <nadav.dotanwork@gmail.com>
2026-08-15 13:56:20 +0300

    patch

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -122,7 +122,7 @@ export function AppSidebar() {
             </p>
 
             <p className="truncate text-xs text-sidebar-foreground/60">
-              1.2 version
+              version 1.2
             </p>
           </div>
         </div>
